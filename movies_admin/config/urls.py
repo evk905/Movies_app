8c4ff74fd076cef1ea867e3319af4a66f2e0d1f1
Ekaterina Kovalenko <evk_905@inbox.ru>
2024-03-03 23:28:56 +0300

    create app

--- movies_admin/config/urls.py
+++ movies_admin/config/urls.py
@@ -12,8 +12,7 @@ if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns = [
-        path("__debug__/", include(debug_toolbar.urls)),
-    ] + urlpatterns
+                      path("__debug__/", include(debug_toolbar.urls)),
+                  ] + urlpatterns
 
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
